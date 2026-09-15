2f86e8513cea7b24f719f325f2b9e118db2a21d1
LoznyX <yunogasaiprivate@gmail.com>
2026-09-15 09:31:49 +0200

    Klasa Main

--- src/Osoba.java
+++ src/Osoba.java
@@ -4,6 +4,19 @@
         private int wiek;
 
     public Osoba(String imie) {
+ this.imie = imie;
+ wiek=0;
+    }
+
+        public int getWiek() {
+            return wiek;
+        }
+
+        public String getImie() {
+            return imie;
+        }
+
+        public Osoba(String imie, int wiek) {
 
     }
 }
---
+++ src/Uczen.java
@@ -0,0 +1,21 @@
+public class Uczen extends  Osoba {
+
+    private int nrUcznia;
+    private static int LiczbaUczniow = 0;
+
+    public Uczen(String imie, int wiek) {
+        super(imie, wiek);
+        LiczbaUczniow++;
+        nrUcznia = LiczbaUczniow;
+    }
+
+    @Override
+    public String toString() {
+        return "Uczen{" +
+                "Imie" + getImie() +
+                "wiek" + getWiek() +
+                "nrUcznia=" + nrUcznia +
+                '}';
+    }
+}
+
