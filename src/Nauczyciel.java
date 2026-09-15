a6054998f4adce56a577935db3ebb46fb042ac2f
LoznyX <yunogasaiprivate@gmail.com>
2026-09-15 09:51:30 +0200

    Klasa Nauczyciel

---
+++ src/Nauczyciel.java
@@ -0,0 +1,9 @@
+public class Nauczyciel extends Osoba {
+    private String przedmiot;
+
+    public Nauczyciel(String imie, String przedmiot) {
+        super(imie);
+        this.przedmiot = przedmiot;
+    }
+
+}
